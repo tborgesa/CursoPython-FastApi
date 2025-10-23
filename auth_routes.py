210109dcbc6a2abcfe8d804d7d2a6eb8e27261cd
Thiago Borges <thiago.borges.theos@outlook.com>
2025-10-23 10:39:37 -0300

    Aula 02: Requisições e Roteamento da API

---
+++ auth_routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+@auth_router.get("/")
+async def home():
+    """
+    Essa é a rota padrão de autenticação do nosso sistema
+    """
+    return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
