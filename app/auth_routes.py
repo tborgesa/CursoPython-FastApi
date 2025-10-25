800cb9298a805d514f1a16a39cf8267b0d4db803
Thiago Borges <thiago.borges.theos@outlook.com>
2025-10-24 23:44:59 -0300

    Criar Schemas e HttpError (400)

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -13,14 +15,14 @@ async def home():
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         # ja existe um usuario com esse email
-        return {"mensagem": "já existe um usuário com esse email"}
+        raise HTTPException(status_code=400,detail="já existe um usuário com esse email")
     else:
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": "usuário cadastrado com sucesso"}
